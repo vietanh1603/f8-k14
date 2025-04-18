91faf1c77b5f96436ba58343486134d9b95c17d4
NGUYEN TANG HUNG <anhvhf8@fullstack.edu.vn>
2025-04-19 06:12:08 +0700

    put

---
+++ 20250417/js/main.js
@@ -0,0 +1,158 @@
+/*
+alert("Hello World");
+document.write("<h1>Hello World</h1>");
+document.write("Hello World");
+
+console.log("Hello World 1234");
+console.log('12345',54321);
+console.log(1+2, 3-6 ,3*6, -9/0)
+console.log('Hello' + 'World');
+console.log(2 + 'World');
+console.log('2' + 2000);
+ */
+
+// day la comment code
+/*
+https://asciiflow.com/#/
+ */
+/* Bien: variable
+use to store any temp value
+store on RAM
+reset -> remove
+declearr a variable -> 4 way
+var
+let/const( >2015 - EC6)    recommend
+ */
+// khai bao variable (declared variable)
+/*
+var a
+a = 1
+console.log(a)
+
+//initial variable (khoi tao bien)
+var b = 2
+let c = 1
+const d = 1
+a=a+1
+c=a+b
+//d = d + 1
+console.log(a,b,c,d)
+console. log(window.a, window.b)
+
+ */
+
+/*
+operator(toan tu) + - * /
+number + number = number
+number - number = number
+number * number = number
+number / number = number
+
+* RAW STRING
+string + string = string
+string - string = NaN (Not a Number)
+string * string = NaN
+string + string = NaN
+
+*STRING LINK NUMBER
+* string + string = string
+* string - string = number
+* string * string = number
+* string / string = number
+*
+Note: JS try ti convert your operator to correct operator -> should not depend on it
+( JS co gang thuc hien cac phep tinh -> khong nen phu thuoc vao no )
+
+ phep luy thua:
+ a**2 = a *a
+ a ** 3 = a * a * a
+ */
+// console.log(25 **(1/2))
+//chia lay du
+/*
+console.log(10%3)
+console.log(10/3)
+ */
+//let a=1
+//a++
+//++a
+//console.log(a++) //log a truoc roi moi tang a 1 don vi
+//console.log(++a) // cong truoc roi moi log
+ //let a=5
+//const b= a + ++a + a++ + ++a +a-- - --a
+// 5 + 6 + 6 + 8 + 8 - 6
+
+/* comparison operator
+== / === (*) (
+>
+<
+>=
+<=
+!= / !==
+? not sure
+ */
+//console.log(a==b, a===b)
+
+
+/*  data type
+ string
+ number
+ bool
+ null / undefine/ unknown /
+ object
+ function
+to check data type: typeof (<variable>)
+
+ */
+//vd
+// const a = undefined
+// console.log(typeof a)
+/*
+concat: convert data type
+
+Datatype(<variable>)
+
+string<-> number
+    length of string > 0 --> true
+    length of string = 0 --> false
+
+    number --> bool
+    0 --> false
+    != --> true
+
+    bool --> number
+    true --> 1
+    false --> 0
+
+    bool --> string
+    true --> true
+    false --> false
+ */
+let a=123435
+a = String(a)
+console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
