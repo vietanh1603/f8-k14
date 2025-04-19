ab30e4303325cea4bad89d461245d4cac3513160
NGUYEN TANG HUNG <anhvhf8@fullstack.edu.vn>
2025-04-19 22:02:51 +0700

    put

---
+++ 20251904/main.js
@@ -0,0 +1,168 @@
+/*
+conditional statement (cau lenh dieu kien)
+if / else / else if
+syntax
+
+if (condition <compere> (so sanh)) {
+code
+code
+code
+}
+{
+code
+code
+}     (day la block code)
+
+if (condition) {
+your code
+}  ---> if (condition) your code
+ */
+/*
+const name= 'nam'
+
+if (name === 'nam') console.log('hello nam')
+
+let a = 100
+if ((a % 2) === 0) console.log('so chan')
+// cach khac
+*/
+
+
+//const a= 100
+//console.log(a % 8)
+//console.log(Boolean(a % 8))
+//console.log(a % 8 === true)
+//console.log(a % 8 === false)
+
+// if (!(a % 2)) --> Bool(a % 2) -->false --> true
+//if (a % 2 === 0) console.log('so chan')
+
+/*
+let a =''
+console.log(a === null)
+console.log(a === Boolean(a))
+
+if (!a) = 20
+console.log(a)
+ */
+
+//const b = 31
+/*
+if (b % 2 === 0) console.log('so chan')
+else console.log('so le')
+ */
+ // chia 3 du 0 --> du 0
+// chia 3 du 1 --> du 1
+// chia 3 du 2 --> du 2
+
+/*
+if (a %  3 === 0) console.log('so chan')
+else if (a %  3 === 1) console.log('so le')
+else console.log('so le')
+
+switch (a % 3) {
+    case 1:
+        console.log('du 1')
+    case 2:
+        console.log('du 2')
+    case 3:
+        console.log('du 3')
+    default
+        console.log('khong du')
+}
+ */
+
+// console.log('du ' a % 3)
+/*
+
+
+
+
+                                    ┌───────────────────────┐
+                                    │    2                  │
+                                    │  ax  + bx + c         │
+                                    │                       │
+                                    └───────────┬───────────┘
+                                                │
+                                                │
+                                                │
+                                 │              ▼
+                                 ├────────────────────────────────┐
+                                 │              2                 │
+                                 │  dental =   b  - 4ac           │
+                    ┌────────────┼                                ┼────────────────────┐
+                    │            │                                │                    │
+                    │            └─────────────┬──────────────────┴─                   ▼
+                    │                          │                       ┌───────────────────────┐
+                    │                          │                       │                       │
+                    ▼                          ▼                       │                       │
+         ┌──────────────────┐         ┌───────────────────┐            │    denta > 0          │
+         │                  │         │                   │            │                       │
+         │ denta=0          │         │  denta <  0       │            │                       │
+         └──────────────────┘         └───────────────────┘            └───────────────────────┴─
+
+
+
+
+ */
+ /*const a = 3, b=-2, c=5
+delta=b**2 -4*a*c
+if (delta > 0) console.log('vn')
+    else if (delta === 0) console.log('x=',-b/2*a)
+    else {
+        x1 = (-b -delta**0.5)/(2*a)
+        x2 = (-b +delta**0.5)/(2*a)
+    console.log('x1=',x1, 'x2=',x2)
+}
+  */
+
+
+/*
+ --------------- Loop--------------------
+ muc re-use code
+ for
+     for
+        for of
+         forEach
+         map
+         filter
+         reduce
+while
+
+do while (do not use)
+
+
+
+-----------------vong lap for truyen thong -------------------
+
+syntax
+
+for (init, condition; next step) --> for (khoi tao, dieu kien, buoc tiep the) {
+block code
+}
+ */
+/*
+let sum= 0
+ for (let i=0; i<100; i = i+1) {
+     console.log(i)
+     sum = sum + i
+
+ }
+ console.log(sum)
+ */
+const a = 190
+for (let i=2; i<=a; i = i+1) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
