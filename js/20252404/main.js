363320d27d4e8cfb17ed6301da90e1a449f2c86d
NGUYEN TANG HUNG <anhvhf8@fullstack.edu.vn>
2025-04-24 22:03:29 +0700

    put

---
+++ js/20252404/main.js
@@ -0,0 +1,100 @@
+
+
+
+// ----------------------- chua btvn----------------------------------
+
+
+
+// let count = 0;
+// const arrb3 = [132,34,6435,24,64,234,543,45,63,65];
+// for (let j = 0; j < arrb3.length; j++) {
+//     for (let i = 0; i < arrb3.length - 1 - j; i++) {
+//         const curValue = arrb3[i];
+//         const nextValue = arrb3[i + 1];
+//
+//         if(curValue > nextValue) {
+//             arrb3[i] = nextValue;
+//             arrb3[i + 1] = curValue;
+//         }
+//         count++;
+//     }
+// }
+// console.log(arrb3);
+// console.log(count);
+
+
+
+
+//-----------------------HAM(function)--------------------
+/*
+y = x+1
+f(x) = x + 1
+x = 1 --> f(1) = 2
+ */
+
+// function test(x,y) {
+//     const a = 1
+//     console.log('test x = ', x , 'y = ', y)
+//
+// }
+// console.log(test(100, 800))
+
+/*------------ ham co dien -----------------------
+function test (x) {
+if (x > 5 ) console.log(x)
+tuong tu
+if ( x < 5 ) return
+console.log(x)
+ */
+
+
+//----------------------- 2015 ----------------------------
+// const log = (x) => {
+//     console.log(x)
+// }
+//
+// log(100)
+
+//----------------cach khac-------------------
+//const log = (x) => x || 100 //
+
+//console.log(log('1000'))
+
+// const test = (x) => {
+//      return x
+//     console.log(x)
+// }
+
+// const animal = {
+//     name: 'dog',
+//     legs: 4 ,
+//     speaking () {
+//         console.log('hihi')
+//     } ,
+//     eating () {
+//         console.log('com')
+//     }
+// }
+// console.log(animal.name)
+
+
+// ------------------------ Array Object------------------------------
+const member = [
+    { id: 1, name: 'nguyen', age: 23 },
+    { id: 2, name: 'van', age: 23 },
+]
+console.log(member[1].age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
